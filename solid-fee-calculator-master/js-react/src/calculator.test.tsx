b326b12b1286df1e9e357cdc9ef17f1ad5b61304
Stephen Panzer <stephenp@gurutechnologies.net>
2023-12-07 20:59:25 -0700

    Exhaustive test cases

--- solid-fee-calculator-master/js-react/src/calculator.test.tsx
+++ solid-fee-calculator-master/js-react/src/calculator.test.tsx
@@ -1,16 +1,70 @@
+import { AdTypeEnum } from './Enums/AdTypeEnum';
+import { UserTypeEnum } from './Enums/UserTypeEnum';
 import { Calculator } from './calculator';
 import moment from 'moment';
 
+const testCases = [
+    {
+        userType: UserTypeEnum.Normal,
+        adType: AdTypeEnum.Auction,
+        price: 10,
+        endDate: moment().format("YYYY-MM-DD"),
+        expected: 25
+    },
+    {
+        userType: UserTypeEnum.Normal,
+        adType: AdTypeEnum.Auction,
+        price: 10,
+        endDate: moment().subtract(1, "days").format("YYYY-MM-DD"),
+        expected: 35
+    },
+    {
+        userType: UserTypeEnum.Normal,
+        adType: AdTypeEnum.BuyItNow,
+        price: 10,
+        endDate: moment().format("YYYY-MM-DD"),
+        expected: 35
+    },
+    {
+        userType: UserTypeEnum.Normal,
+        adType: AdTypeEnum.BuyItNow,
+        price: 10,
+        endDate: moment().subtract(1, "days").format("YYYY-MM-DD"),
+        expected: 45
+    },
+    {
+        userType: UserTypeEnum.Company,
+        adType: AdTypeEnum.Auction,
+        price: 10,
+        endDate: moment().format("YYYY-MM-DD"),
+        expected: 20
+    },
+    {
+        userType: UserTypeEnum.Company,
+        adType: AdTypeEnum.Auction,
+        price: 10,
+        endDate: moment().subtract(1, "days").format("YYYY-MM-DD"),
+        expected: 30
+    },
+    {
+        userType: UserTypeEnum.Company,
+        adType: AdTypeEnum.BuyItNow,
+        price: 10,
+        endDate: moment().format("YYYY-MM-DD"),
+        expected: 25
+    },
+    {
+        userType: UserTypeEnum.Company,
+        adType: AdTypeEnum.BuyItNow,
+        price: 10,
+        endDate: moment().subtract(1, "days").format("YYYY-MM-DD"),
+        expected: 35
+    },
+];
 describe("Calculator", () => {
-  it('Can calculate fee', () => {
-    const calc = new Calculator();
-    const fee = calc.getFee({
-      userType: 0,
-      itemType: 0,
-      price: 10,
-      endDate: moment().format("YYYY-MM-DD")
+    test.each(testCases)("Can calculate fee", (testCase) => {
+        const calc = new Calculator();
+        const fee = calc.getFee(testCase);
+        expect(fee).toBe(testCase.expected);
     });
-
-    expect(fee).toBe(25);
-  });
 });
