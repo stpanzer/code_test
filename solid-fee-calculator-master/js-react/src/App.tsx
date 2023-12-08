87baaf95d0741066c806baa2bd1215b4b361d8a1
Stephen Panzer <stephenp@gurutechnologies.net>
2023-12-07 22:29:02 -0700

    Move all state changes into reducer function, convert to style in react docs

--- solid-fee-calculator-master/js-react/src/App.tsx
+++ solid-fee-calculator-master/js-react/src/App.tsx
@@ -1,177 +1,185 @@
-import React, { Component, FormEventHandler } from "react";
+import React, { Component, FormEventHandler, useReducer } from "react";
 import "./App.css";
 import { Calculator } from "./calculator";
 import moment from "moment";
 import { FeeItem } from "./Interfaces/FeeItem";
 import { ItemList } from "./ItemList/ItemList";
-class App extends Component<
-  {},
-  {
-    items: FeeItem[];
-    total: number;
-    newItem: FeeItem;
-  }
-> {
-  constructor(props: {} | Readonly<{}>) {
-    super(props);
 
-    this.state = {
-      items: [],
-      total: 0,
-      newItem: {
-        adType: -1,
-        userType: -1,
-        price: 100,
-        endDate: moment().format("YYYY-MM-DD"),
-      },
-    };
-  }
+interface AppState {
+  items: FeeItem[];
+  total: number;
+  newItem: FeeItem;
+}
+const calculator = new Calculator();
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    items: [],
+    total: 0,
+    newItem: {
+      adType: -1,
+      userType: -1,
+      price: 100,
+      endDate: moment().format("YYYY-MM-DD"),
+    },
+  });
 
-  onItemTypeChange: FormEventHandler = (event) => {
-    const { newItem } = this.state;
-
-    this.setState({
-      newItem: {
-        userType: newItem.userType,
-        price: newItem.price,
-        endDate: newItem.endDate,
-        adType: parseInt((event.target as HTMLSelectElement).value, 10),
-      },
+  const onItemTypeChange: FormEventHandler = (event) => {
+    dispatch({
+      type: "update_ad_type",
+      payload: parseInt((event.target as HTMLSelectElement).value, 10),
     });
   };
 
-  onUserTypeChanged: FormEventHandler = (event) => {
-    const { newItem } = this.state;
-
-    this.setState({
-      newItem: {
-        adType: newItem.adType,
-        price: newItem.price,
-        endDate: newItem.endDate,
-        userType: parseInt((event.target as HTMLSelectElement).value, 10),
-      },
+  const onUserTypeChanged: FormEventHandler = (event) => {
+    dispatch({
+      type: "update_user_type",
+      payload: parseInt((event.target as HTMLSelectElement).value, 10),
     });
   };
 
-  onNewItemSubmit: FormEventHandler = (event) => {
+  const onNewItemSubmit: FormEventHandler = (event) => {
     event.preventDefault();
-
-    const formElement = event.target as HTMLFormElement;
-    const isValid = formElement.checkValidity();
-
-    if (!isValid) {
-    } else {
-      const calc = new Calculator();
-      const { newItem } = this.state;
-      const fee = calc.getFee(newItem);
-
-      // update total
-      this.setState({
-        total: this.state.total + fee,
-        items: [...this.state.items, newItem],
-      });
-    }
+    dispatch({ type: "add_item", payload: state.newItem });
   };
 
-  onPriceChanged: FormEventHandler = (event) => {
-    const { newItem } = this.state;
-
-    this.setState({
-      newItem: {
-        adType: newItem.adType,
-        userType: newItem.userType,
-        endDate: newItem.endDate,
-        price: parseInt((event.target as HTMLInputElement).value, 10),
-      },
+  const onPriceChanged: FormEventHandler = (event) => {
+    dispatch({
+      type: "update_price",
+      payload: parseInt((event.target as HTMLInputElement).value, 10),
     });
   };
 
-  onEndDateChanged: FormEventHandler = (event) => {
-    const { newItem } = this.state;
-
-    this.setState({
-      newItem: {
-        adType: newItem.adType,
-        userType: newItem.userType,
-        price: newItem.price,
-        endDate: (event.target as HTMLInputElement).value,
-      },
+  const onEndDateChanged: FormEventHandler = (event) => {
+    dispatch({
+      type: "update_end_date",
+      payload: (event.target as HTMLInputElement).value,
     });
   };
 
-  render() {
-    const { newItem, total, items } = this.state;
+  const { newItem, total, items } = state;
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Solid Fee Calculator</h1>
-        </header>
-        <div className="App-page">
-          <h2>Items</h2>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to Solid Fee Calculator</h1>
+      </header>
+      <div className="App-page">
+        <h2>Items</h2>
 
-          <ItemList items={items}></ItemList>
+        <ItemList items={items}></ItemList>
 
-          <p>Total fees: ${total} </p>
+        <p>Total fees: ${total} </p>
 
-          <h3>Register new item</h3>
-          <form className="New-item-form" onSubmit={this.onNewItemSubmit}>
-            <div className="form-group">
-              <label>You are</label>
-              <select
-                className="form-control"
-                id="itemType"
-                defaultValue=""
-                required
-                onChange={this.onUserTypeChanged}
-              >
-                <option value="">Select</option>
-                <option value="0">Person</option>
-                <option value="1">Company</option>
-              </select>
-            </div>
+        <h3>Register new item</h3>
+        <form className="New-item-form" onSubmit={onNewItemSubmit}>
+          <div className="form-group">
+            <label>You are</label>
+            <select
+              className="form-control"
+              id="itemType"
+              defaultValue=""
+              required
+              onChange={onUserTypeChanged}
+            >
+              <option value="">Select</option>
+              <option value="0">Person</option>
+              <option value="1">Company</option>
+            </select>
+          </div>
 
-            <div className="form-group">
-              <label>Item Type</label>
-              <select
-                className="form-control"
-                id="itemType"
-                defaultValue=""
-                required
-                onChange={this.onItemTypeChange}
-              >
-                <option value="">Select </option>
-                <option value="0">Auction</option>
-                <option value="1">Buy it now</option>
-              </select>
-            </div>
+          <div className="form-group">
+            <label>Item Type</label>
+            <select
+              className="form-control"
+              id="itemType"
+              defaultValue=""
+              required
+              onChange={onItemTypeChange}
+            >
+              <option value="">Select </option>
+              <option value="0">Auction</option>
+              <option value="1">Buy it now</option>
+            </select>
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="itemType">Price</label>
-              <input
-                className="form-control"
-                type="number"
-                value={newItem.price}
-                onChange={this.onPriceChanged}
-              />
-            </div>
+          <div className="form-group">
+            <label htmlFor="itemType">Price</label>
+            <input
+              className="form-control"
+              type="number"
+              value={newItem.price}
+              onChange={onPriceChanged}
+            />
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="itemType">End date</label>
-              <input
-                className="form-control"
-                type="date"
-                value={newItem.endDate}
-                onChange={this.onEndDateChanged}
-              />
-            </div>
+          <div className="form-group">
+            <label htmlFor="itemType">End date</label>
+            <input
+              className="form-control"
+              type="date"
+              value={newItem.endDate}
+              onChange={onEndDateChanged}
+            />
+          </div>
 
-            <input type="submit" className="btn btn-primary" value="Submit" />
-          </form>
-        </div>
+          <input type="submit" className="btn btn-primary" value="Submit" />
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default App;
+type actionTypes =
+  | "add_item"
+  | "update_price"
+  | "update_end_date"
+  | "update_user_type"
+  | "update_ad_type";
+
+function reducer(state: AppState, action: { type: actionTypes; payload: any }) {
+  switch (action.type) {
+    case "add_item":
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          { ...action.payload, price: calculator.getFee(action.payload) },
+        ],
+        total: calculator.getFee(action.payload),
+      };
+    case "update_price":
+      return {
+        ...state,
+        newItem: {
+          ...state.newItem,
+          price: action.payload,
+        },
+      };
+    case "update_end_date":
+      return {
+        ...state,
+        newItem: {
+          ...state.newItem,
+          endDate: action.payload,
+        },
+      };
+    case "update_user_type":
+      return {
+        ...state,
+        newItem: {
+          ...state.newItem,
+          userType: action.payload,
+        },
+      };
+    case "update_ad_type":
+      return {
+        ...state,
+        newItem: {
+          ...state.newItem,
+          adType: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+}
