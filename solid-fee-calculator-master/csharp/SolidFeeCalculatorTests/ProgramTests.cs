c4e3c1d59d94dd14dac1763cfb3f0affb5974ca4
Stephen Panzer <stephenp@gurutechnologies.net>
2023-12-07 20:21:11 -0700

    Remove unused test cruft

--- solid-fee-calculator-master/csharp/SolidFeeCalculatorTests/ProgramTests.cs
+++ solid-fee-calculator-master/csharp/SolidFeeCalculatorTests/ProgramTests.cs
@@ -14,55 +14,6 @@ namespace SolidFeeCalculatorTest
     [TestFixture]
     public class ProgramTest
     {
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        [SetUp]
-        public void Setup()
-        {
-        }
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
-        static DateTime today = DateTime.Today;
         /// <summary>
         ///A test for CalculateFee
         ///</summary>
@@ -73,6 +24,7 @@ namespace SolidFeeCalculatorTest
             Assert.That(expected == actual);
         }
 
+        static DateTime today = DateTime.Today;
         static TestCaseData[] CalculateFeeTestCases = [
             new TestCaseData(UserTypeEnum.Normal, AdTypeEnum.Auction, 100m, today, 115),
             new TestCaseData(UserTypeEnum.Normal, AdTypeEnum.Auction, 100m, today.AddDays(-1), 125),
